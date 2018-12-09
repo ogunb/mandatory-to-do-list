1e30699f525f948a9367eace5bc13c5892c90cd4
ogunb <babacanogun@gmail.com>
2018-12-10 00:12:24 +0300

    add todo list components

---
+++ src/Components/AddTodo/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+	render() {
+		return (
+			<div>
+				<p>sadfıojasdoıgasdf</p>
+			</div>
+		);
+	}
+}
+
+export default AddTodo;
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Today from './Today';
+import Todo from './Todo';
+import AddTodo from './AddTodo';
+import './index.css';
 
 class App extends Component {
+	state = {
+		list: [
+			{ content: 'Lorem ipsum dolor', isDone: false },
+			{ content: 'dude no what no', isDone: true }
+		]
+	};
+	handleChange = id => {
+		const { list } = this.state;
+		[list[id].isDone] = [!list[id].isDone];
+		this.setState({
+			...this.state,
+			list
+		});
+	};
 	render() {
+		const { list } = this.state;
 		return (
 			<div className="App">
-				<p>sdafaısjdgoıasjıdf</p>
+				<Today />
+				<h4 className="header">To do List</h4>
+				<div className="theLists">
+					{list.map((todo, index) => (
+						<Todo
+							content={todo.content}
+							isDone={todo.isDone}
+							key={index}
+							id={index}
+							handleChange={this.handleChange}
+						/>
+					))}
+				</div>
+				<AddTodo />
 			</div>
 		);
 	}
