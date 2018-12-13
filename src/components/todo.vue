ffed405b99619f026213ccd457322ee896e59a97
ogunb <babacanogun@gmail.com>
2018-12-14 01:05:16 +0300

    asdfasdga

---
+++ src/components/todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="listWrapper">
+    <div class="theList" v-bind:class="{ isDone: isDone }" @click="handleIsDone(index)">
+      <div>
+        <label class="theList__label" for="status">{{ content }}</label>
+      </div>
+      <input type="checkbox" class="status">
+      <span class="check">
+        <svg width="18px" height="18px" viewBox="0 0 18 18">
+          <path
+            d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"
+          ></path>
+          <polyline points="1 9 7 14 15 4"></polyline>
+        </svg>
+      </span>
+    </div>
+    <delete-todo v-bind="{removeTodo, index}"></delete-todo>
+  </div>
+</template>
+
+<script>
+import DeleteTodo from "./DeleteTodo.vue";
+
+export default {
+  name: "TodoList",
+  props: ["content", "isDone", "handleIsDone", "removeTodo", "index"],
+  components: {
+    DeleteTodo
+  }
+};
+</script>
+
